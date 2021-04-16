3743680c848bf5c46ffd03bde6e1d12b510edd1c
Anna <locked_forever@mail.ru>
2021-04-16 17:26:47 +0300

    feat: добавить скрипт

--- scripts/index.js
+++ scripts/index.js
@@ -2,10 +2,10 @@ const openPopupButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const closePopupButton = document.querySelector('.popup__close-button');
 
-openPopupButton.addEventListener("click", function (){
+function togglePopup (){
     popup.classList.toggle('popup_is-opened');
-});
+}
 
-closePopupButton.addEventListener("click", function (){
-    popup.classList.toggle('popup_is-opened');
-});
+openPopupButton.addEventListener("click", togglePopup);
+
+closePopupButton.addEventListener("click", togglePopup);
