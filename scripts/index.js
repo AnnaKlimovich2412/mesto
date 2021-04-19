ae95949b7529a6eac7b17f2f98e4729ca4f83529
Anna Klimovich <locked_forever@mail.ru>
2021-04-19 23:43:58 +0300

    fix:исправить замечания

--- scripts/index.js
+++ scripts/index.js
@@ -1,27 +1,49 @@
-const openPopupButton = document.querySelector('.profile__edit-button');
-const popup = document.querySelector('.popup');
-const closePopupButton = document.querySelector('.popup__close-button');
-const formElement = document.querySelector('.popup__container');
-const inputName = formElement.querySelector('.popup__field-name');
-const inputProfession = formElement.querySelector('.popup__field-profession');
+// profile information
 const profileName = document.querySelector('.profile__name');
 const profileProfession = document.querySelector('.profile__profession');
 
-function togglePopup (){
-    popup.classList.toggle('popup_is-opened');
+//popUp
+const popup = document.querySelector('.popup');
+const popupContainer = document.querySelector('.popup__container');
+
+//buttons
+const openPopupButton = document.querySelector('.profile__edit-button');
+const closePopupButton = document.querySelector('.popup__close-button');
+
+// form fields
+const formElement = document.querySelector('.popup__container');
+const inputName = formElement.querySelector('.popup__field-name');
+const inputProfession = formElement.querySelector('.popup__field_profession');
+
+
+// event listeners
+openPopupButton.addEventListener("click", openPopup);
+closePopupButton.addEventListener("click", closePopup);
+formElement.addEventListener('submit', formSubmitHandler);
+
+
+
+// open dialog window
+function openPopup() {
+  popup.classList.add('popup_is-opened');
+  inputName.value = profileName.textContent;
+  inputProfession.value = profileProfession.textContent;
 }
 
-function formSubmitHandler (evt) {
-    evt.preventDefault();
-
-    let inputNameValue = inputName.value;
-    let inputProfessionValue = inputProfession.value;
-
-    profileName.textContent = inputNameValue;
-    profileProfession.textContent = inputProfessionValue;
-    togglePopup();
+// close dialog window
+function closePopup() {
+  popup.classList.remove('popup_is-opened');
 }
 
-formElement.addEventListener('submit',formSubmitHandler);
-openPopupButton.addEventListener("click", togglePopup);
-closePopupButton.addEventListener("click", togglePopup);
+// set new value and close dialog
+function formSubmitHandler(evt) {
+  evt.preventDefault();
+
+  profileName.textContent = inputName.value;
+  profileProfession.textContent = inputProfession.value;
+
+  closePopup()
+}
+
+
+
