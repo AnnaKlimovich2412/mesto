21f60ef691b1780f71c67fbba0300688b26662d1
Anna <locked_forever@mail.ru>
2021-04-19 17:40:08 +0300

    fix: исправить замечания

--- scripts/index.js
+++ scripts/index.js
@@ -1,33 +1,27 @@
 const openPopupButton = document.querySelector('.profile__edit-button');
 const popup = document.querySelector('.popup');
 const closePopupButton = document.querySelector('.popup__close-button');
+const formElement = document.querySelector('.popup__container');
+const inputName = formElement.querySelector('.popup__field-name');
+const inputProfession = formElement.querySelector('.popup__field-profession');
+const profileName = document.querySelector('.profile__name');
+const profileProfession = document.querySelector('.profile__profession');
 
 function togglePopup (){
     popup.classList.toggle('popup_is-opened');
 }
 
-openPopupButton.addEventListener("click", togglePopup);
-
-closePopupButton.addEventListener("click", togglePopup);
-
-const formElement = document.querySelector('.popup__container');
-
-
 function formSubmitHandler (evt) {
     evt.preventDefault();
-    const inputName = formElement.querySelector('.popup__field-name');
-    const inputProfession = formElement.querySelector('.popup__field-profession');
 
     let inputNameValue = inputName.value;
     let inputProfessionValue = inputProfession.value;
 
-    const profileName = document.querySelector('.profile__name');
-    const profileProfession = document.querySelector('.profile__profession');
-
     profileName.textContent = inputNameValue;
     profileProfession.textContent = inputProfessionValue;
     togglePopup();
-
 }
 
 formElement.addEventListener('submit',formSubmitHandler);
+openPopupButton.addEventListener("click", togglePopup);
+closePopupButton.addEventListener("click", togglePopup);
